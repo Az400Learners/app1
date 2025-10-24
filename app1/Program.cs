490762e6a401994be611b40cf8a06ffa192b1a9d
raja <arajavignesh@gmail.com>
2025-10-24 07:22:12 +0100

    Update Program.cs

--- app1/Program.cs
+++ app1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!!!!$$$ d");
+app.MapGet("/", () => "Hello World!!!!$$$ dillo");
 
 app.Run();
